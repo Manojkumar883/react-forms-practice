07f2abc8a7acc9b3491f4aa21fc9e83d10513c63
Manoj kumar <manoj.bhoosani@gmail.com>
2023-01-25 10:16:32 +0100

    first part completed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
 import { useState } from "react";
 import "./App.css";
 
+const intialFormState = {
+  name: '',
+  address :'',
+  phone: '',
+  email:''
+}
 export default function App() {
   
   //TODO: Add your state fields here
+ const [formState, setFormState] = useState(intialFormState)
+//  const [address, setAddress] = useState(intialFormState.address)
 
+ const handleChange = (event) => {
+  const value = event.target.value
+  const name = event.target.name
+  const type = event.target.type
+
+
+  console.log("handleChange", name, type, value)
+
+  const newFormState = {...formState}
+  if(name === 'name') {
+    newFormState.name = value
+  }
+  if(name === 'address') {
+    newFormState.address = value
+  }
+  if(name === 'email') {
+    newFormState.email = value
+  }
+  if(name === 'phone') {
+    newFormState.phone = value
+  }
+  setFormState(newFormState)
+ 
+ }
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+
+  console.log("form submitted.name=",formState.name , "address =",formState.address,
+  formState.email, formState.phone )
+  setFormState(intialFormState)
+}
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit = {handleSubmit}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input type="text" name="name" value = {formState.name} onChange ={handleChange}required />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input type="text" name="address" value = {formState.address} onChange = {handleChange}/>
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input type="tel" name="phone" value = {formState.phone} onChange = {handleChange} />
           </label>
 
           <label>
             Email
-            <input type="email" name="email" />
+            <input type="email" name="email"  value = {formState.email} onChange = {handleChange}/>
           </label>
         </div>
 
